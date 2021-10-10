2f7f20293f76e12bf6307e17d1b870ac4a156ccb
nekobutton <japanztp@gmail.com>
2021-10-11 00:24:40 +0800

    Added more UI screen

--- 2D RollBall/Assets/Script/ManagerSystem/MapManager.cs
+++ 2D RollBall/Assets/Script/ManagerSystem/MapManager.cs
@@ -1,16 +1,30 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MapManager : MonoBehaviour
 {
    public String mapTitle = "地图名";
+   [TextArea]
+   public String mapDescription = "";
+   public int levelCode;
+   public int levelSubCode;
 
+   private Text titleUI;
+   private TextMeshProUGUI descriptionUI;
+   
    private void Awake()
    {
-      var title =  GameObject.Find("GoalText");
-      title.GetComponent<Text>().text = mapTitle;
+      titleUI =  GameObject.Find("GoalText").GetComponent<Text>();
+      descriptionUI = GameObject.Find("DescriptionTextTMP").GetComponent<TextMeshProUGUI>();
+   }
+
+   private void Start()
+   {
+      titleUI.text = mapTitle;
+      descriptionUI.text = mapDescription;
    }
 }
